9516b1801a6a1185766c6dfaf66bca552f564542
pawansrirama <118757743+pawansrirama@users.noreply.github.com>
2023-04-24 23:41:59 +0530

    Update settings.py

--- videosearch/settings.py
+++ videosearch/settings.py
@@ -125,6 +125,4 @@ MEDIA_ROOT = BASE_DIR/'media'
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
-CSRF_COOKIE_SECURE = True
-
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
