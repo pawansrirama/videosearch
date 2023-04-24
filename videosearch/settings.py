d79b248f59b1d225e2330ef425742ef977dd6071
pawansrirama <118757743+pawansrirama@users.noreply.github.com>
2023-04-24 23:34:25 +0530

    Update settings.py

--- videosearch/settings.py
+++ videosearch/settings.py
@@ -122,10 +122,6 @@ STATIC_URL = 'static/'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR/'media'
 
-
-SESSION_COOKIE_SECURE = True
-CSRF_COOKIE_SECURE = True
-SECURE_SSL_REDIRECT = True
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
