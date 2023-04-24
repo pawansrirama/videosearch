993486c38ca5f46fb3ea767a75bcdbb139753d74
pawansrirama <118757743+pawansrirama@users.noreply.github.com>
2023-04-25 00:23:03 +0530

    Update settings.py

--- videosearch/settings.py
+++ videosearch/settings.py
@@ -121,6 +121,7 @@ USE_TZ = True
 STATIC_URL = 'static/'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR/'media'
+STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.StaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
