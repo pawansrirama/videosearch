1ecd9038c50b4cea362b7feabf510a541d300020
pawansrirama <118757743+pawansrirama@users.noreply.github.com>
2023-04-25 11:23:32 +0530

    Update urls.py

--- videosearch/urls.py
+++ videosearch/urls.py
@@ -24,8 +24,7 @@ urlpatterns = [
     path('admindj/', admin.site.urls),
     path('',include('user.urls')),
     path('',include('myadmin.urls')),
-    re_path(r'^media/(?P<path>.*)$', serve,{'document_root': settings.MEDIA_ROOT}), 
-    re_path(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
+    re_path(r'^media/(?P<path>.*)$', serve,{'document_root': settings.MEDIA_ROOT})
      
 ]
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
